087ad6ad47307f64b1f046af6486ff0a797a56b7
Jesse Estomago <jesse@scorp.io>
2013-07-08 09:52:34 +0200

    Update metadata

--- cookbooks/lb_nginx/metadata.rb
+++ cookbooks/lb_nginx/metadata.rb
@@ -5,3 +5,10 @@ license          'All rights reserved'
 description      'Installs/Configures lb_nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "rightscale"
+depends "app"
+depends "lb"
+
+recipe "lb_nginx::default",
+  "This loads the required 'lb' resource using the lb_nginx provider."
