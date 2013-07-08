97cdb7577057034e247035ef815aac9c835345f3
Jesse Estomago <jesse@scorp.io>
2013-07-08 13:10:44 +0200

    Modify default recipe

--- cookbooks/lb_nginx/recipes/default.rb
+++ cookbooks/lb_nginx/recipes/default.rb
@@ -16,4 +16,26 @@ end
 log "  Override load balancer to use HAProxy."
 node[:lb][:service][:provider] = "lb_haproxy"
 
+log "  Installing nginx"
+
+# Installs Nginx package.
+package "nginx" do
+  action :install
+end
+
+# Creates Nginx service.
+service "nginx" do
+  supports :reload => true, :restart => true, :status => true, :start => true, :stop => true
+  action :enable
+end
+
+# Creates some common direcotories
+include_recipe "lb_nginx::common_dirs"
+
+# Create enable/disable site scriptt
+include_recipe "lb_nginx::common_scripts"
+
+# Create enable/disable site scriptt
+include_recipe "lb_nginx::common_confs"
+
 rightscale_marker :end
