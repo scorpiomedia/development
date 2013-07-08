c6748e619b4c92c32d7979e03ef618cfb7c162c5
Jesse Estomago <jesse@scorp.io>
2013-07-08 09:42:39 +0200

    Initialized lb_nginx cookbook

---
+++ cookbooks/lb_nginx/metadata.rb
@@ -0,0 +1,7 @@
+name             'lb_nginx'
+maintainer       'Scorpio Interactive'
+maintainer_email 'jesse@scorp.io'
+license          'All rights reserved'
+description      'Installs/Configures lb_nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/lb_nginx/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: lb_nginx
+# Recipe:: default
+#
+# Copyright 2013, Scorpio Interactive
+#
+# All rights reserved - Do Not Redistribute
+#
